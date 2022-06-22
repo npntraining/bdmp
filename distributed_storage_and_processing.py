a1ad7e6e1d3f489e3f21a62c9a87d9006ca5c2b4
NPN Training <npn.npntraining@gmail.com>
2022-06-22 22:07:43 +0530

    Add files via upload

---
+++ distributed_storage_and_processing.py
@@ -0,0 +1,35 @@
+import gdown
+import os
+
+id = "1-Blx0jokYbdZD4WMxdlNqeYXaMLDAF_S"
+output="/home/npntraining/hadoop-2.10.1.zip"
+gdown.download(id=id, output=output, quiet=False)
+
+id = "11-5MynqDJ2sizxaNCcZzpJ4SOygyxpEkD"
+output = "/home/npntraining/hive-3.1.2.zip"
+gdown.download(id=id, output=output, quiet=False)
+
+id = "1oBbj5N8M7-LMsTZTxXCeZEDJvCp57f7w"
+output = "/home/npntraining/bashrc"
+gdown.download(id=id, output=output, quiet=False)
+
+
+os.system('unzip hadoop-2.10.1.zip')
+os.system('rm /home/npntraining/hadoop-2.10.1.zip')
+
+os.system('unzip /home/npntraining/hive-3.1.2.zip')
+os.system('rm /home/npntraining/hive-3.1.2.zip')
+
+os.system('cp /home/npntraining/bashrc ~/.bashrc')
+os.system('rm /home/npntraining/bashrc')
+
+
+id = "1s-7sjTFIjU4OLaJa6-JyBstbcrO7KIy3"
+output="/home/npntraining/spark-3.1.3.zip"
+gdown.download(id=id, output=output, quiet=False)
+
+os.system('unzip spark-3.1.3.zip')
+os.system('rm /home/npntraining/spark-3.1.3.zip')
+
+
+
